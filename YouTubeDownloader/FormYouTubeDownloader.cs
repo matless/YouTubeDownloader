522cb6316d8e76c874587afa2619d10fc0f132d3
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2023-09-22 12:01:46 +0200

    changes

--- YouTubeDownloader/FormYouTubeDownloader.cs
+++ YouTubeDownloader/FormYouTubeDownloader.cs
@@ -1,3 +1,5 @@
+using VideoLibrary;
+
 namespace YouTubeDownloader
 {
     public partial class FormYouTubeDownloader : Form
@@ -7,9 +9,20 @@ namespace YouTubeDownloader
             InitializeComponent();
         }
 
-        private void downloadButton_Click(object sender, EventArgs e)
+        private async void downloadButton_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Button Clicked");
+            //var url = "https://youtube.com/shorts/p03iq190txc?si=xkve9M-X1ZGNxzYG";
+
+            await DownloadFileAsync(textBoxDownload.Text);
+        }
+
+        private async Task DownloadFileAsync(string movieUrl)
+        {
+            var youtube = YouTube.Default;
+            var movie = await youtube.GetVideoAsync(movieUrl);
+            var data = await movie.GetBytesAsync();
+            var destFilePath = @"C:\Users\komp\Desktop\download\" + movie.FullName;
+            await File.WriteAllBytesAsync(destFilePath, data);
         }
     }
 }
